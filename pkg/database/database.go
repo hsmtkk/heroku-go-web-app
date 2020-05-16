6298d0b026f8db435b0924074c3390279c2edfcd
kouki <kouki@localhost>
2020-05-16 15:13:53 +0900

    wip

---
+++ pkg/database/database.go
@@ -0,0 +1,51 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/hsmtkk/heroku-go-web-app/pkg/post"
+)
+
+func New(db *sql.DB) Operator {
+	return &databaseOperatorImpl{db: db}
+}
+
+type Operator interface {
+	Retrieve(id int) (post.Post, error)
+	Create(post.Post) error
+	Update(post.Post) error
+	Delete(post.Post) error
+}
+
+type operatorImpl struct {
+	db *sql.DB
+}
+
+func (impl *operatorImpl) Retrieve(id int) (post.Post, error) {
+	p := post.Post{}
+	err := impl.db.QueryRow("SELECT id, content, author from posts where id = $1", id).Scan(&p.ID, &p.Content, &p.Author)
+	if err != nil {
+		return post.Post{}, err
+	}
+	return p, nil
+}
+
+func (impl *operatorImpl) Create(p post.Post) error {
+	stStr := "INSERT INTO posts (content, author) values ($1, $2) returning id"
+	st, err := impl.db.Prepare(stStr)
+	if err != nil {
+		return err
+	}
+	defer st.Close()
+	return st.QueryRow(p.Content, p.Author).Scan(&p.ID)
+}
+
+func (impl *operatorImpl) Update(p post.Post) error {
+	_, err := impl.db.Exec("UPDATE posts SET content = $2, author = $1 WHERE id = $1", p.ID, p.Content, p.Author)
+	return err
+}
+
+func (impl *operatorImpl) Delete(p post.Post) error {
+	_, err := impl.db.Exec("DELETE FROM posts WHERE id = $1", p.ID)
+	return err
+}
---
+++ pkg/database/database_test.go
@@ -0,0 +1,36 @@
+package database_test
+
+import (
+	"database/sql"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/hsmtkk/heroku-go-web-app/pkg/post"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDatabase(t *testing.T) {
+	tmpDir, _ := ioutil.TempDir("", "database")
+	defer os.RemoveAll(tmpDir)
+	db, _ := sql.Open("sqlite3", "./test.db")
+	defer db.Close()
+	ope := database.New(db)
+
+	err := ope.Create(post.Post{ID: 0, Content: "alpha", Author: "bravo"})
+	assert.Nil(t, err, "should be nil")
+
+	p, err := ope.Retrieve(0)
+	assert.Nil(t, err, "should be nil")
+	assert.Equal(t, "alpha", p.Content, "should be equal")
+	assert.Equal(t, "bravo", p.Author, "should be equal")
+
+	err := ope.Update(post.Post{ID: 0, Content: "charlie", Auhtor: "delta"})
+	assert.Nil(t, err, "should be nil")
+	assert.Equal(t, "charlie", p.Content, "should be equal")
+	assert.Equal(t, "delta", p.Author, "should be equal")
+
+	err := ope.Delete(post)
+	assert.Nil(t, err, "should be nil")
+}
