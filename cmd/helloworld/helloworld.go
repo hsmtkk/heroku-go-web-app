141e07c404939de59a5659803c597bdd63cf761e
kouki <kouki@localhost>
2020-05-18 07:49:08 +0900

    up

--- cmd/helloworld/helloworld.go
+++ cmd/helloworld/helloworld.go
@@ -11,11 +11,16 @@ import (
 )
 
 func main() {
+	for _, pair := os.Environ() {
+		fmt.Println(pair)
+	}
+
 	portStr := os.Getenv("PORT")
 	port, err := strconv.Atoi(portStr)
 	if err != nil {
 		log.Fatal(err)
 	}
+	
 	http.HandleFunc("/", helloworld.Handler)
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		log.Fatal(err)
